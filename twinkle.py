9e151f191cc08c02dc493e5a4a2b38c6f02fb655
LionsPhil <LionsPhil@users.noreply.github.com>
2020-12-12 19:18:52 +0000

    Make the randomness less influential
    ...which helps a lot.
    
    Also allow not every bulb to be a flasher, but this doesn't work very
    well.

--- twinkle.py
+++ twinkle.py
@@ -18,8 +18,10 @@ temps_fil = [0.0] * len(tree) # filament temperatures
 temps_bi = [0.0] * len(tree) # bimetallic strip temperatures
 
 voltage = 0.5 * len(tree) # enough for bulbs to normally run at 50%
+bms_every = 1 # doesn't work very well
 bimetallic_open_temp = 0.5
-heat_rate = 0.15
+heat_rate = 0.1
+heat_variance = 0.05
 heat_transfer_rate = 0.1
 cool_rate = 0.05
 open_resistance = 0.01
@@ -30,9 +32,11 @@ try:
 		for index, pixel in enumerate(tree):
 			# Work out the voltage across this bulb
 			v_drop = voltage * (resistances[index] / total_resistance)
+			# Does this one *have* a bimetallic strip?
+			has_bms = ((index % bms_every) == 0)
 			# Heat filament based on current and noise if not yet shorted
-			if temps_bi[index] < bimetallic_open_temp:
-				temps_fil[index] += (v_drop * heat_rate * random.random())
+			if not has_bms or temps_bi[index] < bimetallic_open_temp:
+				temps_fil[index] += (v_drop * heat_rate * random.uniform(1.0 - heat_variance, 1.0 + heat_variance))
 			# Light it based on temperature
 			pixel.color = incandesce(temps_fil[index])
 			# Transfer heat to the bimetallic strip
@@ -41,7 +45,7 @@ try:
 			# Radiate heat out from the bimetallic strip
 			temps_bi[index] *= 1.0 - cool_rate
 			# Recalculate its resistance for next cycle
-			if temps_bi[index] < bimetallic_open_temp:
+			if not has_bms or temps_bi[index] < bimetallic_open_temp:
 				resistances[index] = temps_fil[index] + 1.0
 			else:
 				resistances[index] = open_resistance
